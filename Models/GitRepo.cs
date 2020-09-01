17f41cb1187752e30b5df4a4728b8bbb7083ed67
HMZ <hamzi1995@gmail.com>
2020-09-02 01:45:08 +0300

    Update #3

---
+++ Models/GitRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HMZSoftwareBlazorWebAssembly.Models
+{
+    public class GitRepo
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public string[] Topics { get; set; }
+    }
+}
