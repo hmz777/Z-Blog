86db22d7f07fea4ce1dc785f431ee428e164ff24
HMZ <hamzi1995@gmail.com>
2020-09-08 18:57:30 +0300

    Update #8

---
+++ Models/ContactModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HMZSoftwareBlazorWebAssembly.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "Name can't be more than {1} characters.")]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(150, ErrorMessage = "Email can't be more than {1} characters.")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(150, ErrorMessage = "Subject can't be more than {1} characters.")]
+        public string Subject { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [StringLength(5000, MinimumLength = 30, ErrorMessage = "Message should be between {2} and {1} characters.")]
+        public string Message { get; set; }
+    }
+}
