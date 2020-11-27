d1f8646fec83223bc18f3a1051f87c43717c1085
HMZ <hamzi1995@gmail.com>
2020-11-27 21:51:27 +0200

    Revert httpclient to singleton.

--- HMZ-Software/Program.cs
+++ HMZ-Software/Program.cs
@@ -16,7 +16,7 @@ namespace HMZSoftwareBlazorWebAssembly
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSingleton<IBlogPostProcessorService, BlogPostProcessor>();
             builder.Services.AddTransient<ITopBarControllerService, TopBarController>();            
 
