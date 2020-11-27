4cd9ebb38af1cc282d186580cef47102db37aeee
HMZ <hamzi1995@gmail.com>
2020-11-27 16:48:56 +0200

    Bump project to .NET 5

--- HMZ-Software/Program.cs
+++ HMZ-Software/Program.cs
@@ -14,9 +14,9 @@ namespace HMZSoftwareBlazorWebAssembly
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSingleton<IBlogPostProcessorService, BlogPostProcessor>();
             builder.Services.AddTransient<ITopBarControllerService, TopBarController>();            
 
