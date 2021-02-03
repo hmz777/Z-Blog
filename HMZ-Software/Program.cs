839444882b0f9d3312c6cd85006f84d823e19494
HMZ <hamzi1995@gmail.com>
2021-02-03 13:11:52 -0500

    Update Program.cs

--- HMZ-Software/Program.cs
+++ HMZ-Software/Program.cs
@@ -18,7 +18,7 @@ namespace HMZSoftwareBlazorWebAssembly
 
             builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSingleton<IBlogPostProcessorService, BlogPostProcessor>();
-            builder.Services.AddTransient<ITopBarControllerService, TopBarController>();            
+            builder.Services.AddTransient<ITopBarControllerService, TopBarController>();
             builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
