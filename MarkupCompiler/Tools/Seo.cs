31400aec2354e565bb9a1d043f452e7b50751d28
HMZ <hamzi1995@gmail.com>
2021-03-02 06:36:48 -0500

    Add lastmod tag to sitemap

--- MarkupCompiler/Tools/Seo.cs
+++ MarkupCompiler/Tools/Seo.cs
@@ -1,4 +1,5 @@
 ﻿using MarkupCompiler.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml;
@@ -47,11 +48,14 @@ namespace MarkupCompiler.Tools
             XmlElement url1 = Sitemap.CreateElement("url", "http://www.sitemaps.org/schemas/sitemap/0.9");
             XmlElement loc1 = Sitemap.CreateElement("loc", "http://www.sitemaps.org/schemas/sitemap/0.9");
             loc1.InnerText = string.Format("{0}", Domain);
+            XmlElement mod1 = Sitemap.CreateElement("lastmod", "http://www.sitemaps.org/schemas/sitemap/0.9");
+            mod1.InnerText = DateTime.Today.ToString("yyyy-MM-dd");
             XmlElement changefreq1 = Sitemap.CreateElement("changefreq", "http://www.sitemaps.org/schemas/sitemap/0.9");
             changefreq1.InnerText = "monthly";
             XmlElement priority1 = Sitemap.CreateElement("priority", "http://www.sitemaps.org/schemas/sitemap/0.9");
             priority1.InnerText = "1";
             url1.AppendChild(loc1);
+            url1.AppendChild(mod1);
             url1.AppendChild(changefreq1);
             url1.AppendChild(priority1);
 
@@ -60,11 +64,14 @@ namespace MarkupCompiler.Tools
             XmlElement url2 = Sitemap.CreateElement("url", "http://www.sitemaps.org/schemas/sitemap/0.9");
             XmlElement loc2 = Sitemap.CreateElement("loc", "http://www.sitemaps.org/schemas/sitemap/0.9");
             loc2.InnerText = string.Format("{0}/Blog", Domain);
+            XmlElement mod2 = Sitemap.CreateElement("lastmod", "http://www.sitemaps.org/schemas/sitemap/0.9");
+            mod2.InnerText = DateTime.Today.ToString("yyyy-MM-dd");
             XmlElement changefreq2 = Sitemap.CreateElement("changefreq", "http://www.sitemaps.org/schemas/sitemap/0.9");
             changefreq2.InnerText = "monthly";
             XmlElement priority2 = Sitemap.CreateElement("priority", "http://www.sitemaps.org/schemas/sitemap/0.9");
             priority2.InnerText = "0.8";
             url2.AppendChild(loc2);
+            url2.AppendChild(mod2);
             url2.AppendChild(changefreq2);
             url2.AppendChild(priority2);
 
@@ -73,11 +80,14 @@ namespace MarkupCompiler.Tools
             XmlElement url3 = Sitemap.CreateElement("url", "http://www.sitemaps.org/schemas/sitemap/0.9");
             XmlElement loc3 = Sitemap.CreateElement("loc", "http://www.sitemaps.org/schemas/sitemap/0.9");
             loc3.InnerText = string.Format("{0}/Blog/Tags", Domain);
+            XmlElement mod3 = Sitemap.CreateElement("lastmod", "http://www.sitemaps.org/schemas/sitemap/0.9");
+            mod3.InnerText = DateTime.Today.ToString("yyyy-MM-dd");
             XmlElement changefreq3 = Sitemap.CreateElement("changefreq", "http://www.sitemaps.org/schemas/sitemap/0.9");
             changefreq3.InnerText = "monthly";
             XmlElement priority3 = Sitemap.CreateElement("priority", "http://www.sitemaps.org/schemas/sitemap/0.9");
             priority3.InnerText = "0.7";
             url3.AppendChild(loc3);
+            url3.AppendChild(mod3);
             url3.AppendChild(changefreq3);
             url3.AppendChild(priority3);
 
@@ -86,11 +96,14 @@ namespace MarkupCompiler.Tools
             XmlElement url4 = Sitemap.CreateElement("url", "http://www.sitemaps.org/schemas/sitemap/0.9");
             XmlElement loc4 = Sitemap.CreateElement("loc", "http://www.sitemaps.org/schemas/sitemap/0.9");
             loc4.InnerText = string.Format("{0}/Site", Domain);
+            XmlElement mod4 = Sitemap.CreateElement("lastmod", "http://www.sitemaps.org/schemas/sitemap/0.9");
+            mod4.InnerText = DateTime.Today.ToString("yyyy-MM-dd");
             XmlElement changefreq4 = Sitemap.CreateElement("changefreq", "http://www.sitemaps.org/schemas/sitemap/0.9");
             changefreq4.InnerText = "monthly";
             XmlElement priority4 = Sitemap.CreateElement("priority", "http://www.sitemaps.org/schemas/sitemap/0.9");
             priority4.InnerText = "0.7";
             url4.AppendChild(loc4);
+            url4.AppendChild(mod4);
             url4.AppendChild(changefreq4);
             url4.AppendChild(priority4);
 
@@ -99,11 +112,14 @@ namespace MarkupCompiler.Tools
             XmlElement url5 = Sitemap.CreateElement("url", "http://www.sitemaps.org/schemas/sitemap/0.9");
             XmlElement loc5 = Sitemap.CreateElement("loc", "http://www.sitemaps.org/schemas/sitemap/0.9");
             loc5.InnerText = string.Format("{0}/Terms", Domain);
+            XmlElement mod5 = Sitemap.CreateElement("lastmod", "http://www.sitemaps.org/schemas/sitemap/0.9");
+            mod5.InnerText = DateTime.Today.ToString("yyyy-MM-dd");
             XmlElement changefreq5 = Sitemap.CreateElement("changefreq", "http://www.sitemaps.org/schemas/sitemap/0.9");
             changefreq5.InnerText = "monthly";
             XmlElement priority5 = Sitemap.CreateElement("priority", "http://www.sitemaps.org/schemas/sitemap/0.9");
             priority5.InnerText = "0.7";
             url5.AppendChild(loc5);
+            url5.AppendChild(mod5);
             url5.AppendChild(changefreq5);
             url5.AppendChild(priority5);
 
@@ -114,12 +130,15 @@ namespace MarkupCompiler.Tools
                 XmlElement url = Sitemap.CreateElement("url", "http://www.sitemaps.org/schemas/sitemap/0.9");
                 XmlElement loc = Sitemap.CreateElement("loc", "http://www.sitemaps.org/schemas/sitemap/0.9");
                 loc.InnerText = string.Format("{0}/Blog/Post/{1}", Domain, Post.FileName);
+                XmlElement mod = Sitemap.CreateElement("lastmod", "http://www.sitemaps.org/schemas/sitemap/0.9");
+                mod.InnerText = Post.DateUpdated.ToString("yyyy-MM-dd");
                 XmlElement changefreq = Sitemap.CreateElement("changefreq", "http://www.sitemaps.org/schemas/sitemap/0.9");
                 changefreq.InnerText = "Weekly";
                 XmlElement priority = Sitemap.CreateElement("priority", "http://www.sitemaps.org/schemas/sitemap/0.9");
                 priority.InnerText = "0.9";
 
                 url.AppendChild(loc);
+                url.AppendChild(mod);
                 url.AppendChild(changefreq);
                 url.AppendChild(priority);
 
